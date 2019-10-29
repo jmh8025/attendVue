4b116b1ed9d05e5c5adcf78909b202fbe7fe39bc
error-404 <jjjmnio@naver.com>
2019-10-29 17:42:12 +0900

    d

--- components/StudentList.vue
+++ components/StudentList.vue
@@ -1024,7 +1024,7 @@ export default {
         onDecode (decodedString) {
             // alert(decodedString)
             this.search = decodedString;
-            showModal = false;
+            this.showModal = false;
         }
     },
     fetch(){
